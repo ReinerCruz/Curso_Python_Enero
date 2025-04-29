f39aa1d18550634ca1ed46af676890aa4e07a7f0
ReinerCruz <a20196816@pucp.edu.pe>
2025-04-29 14:21:11 -0500

    Realizar último cambio ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,6 +2,18 @@
 """
 Created on Tue Apr 29 11:52:58 2025
 
-@author: Mi Equipo
+@author: Reiner Gaga
 """
+# %% Establecemos el directorio
+
+import os
+os.chdir(r"C:\Users\Mi Equipo\Curso_Python_Enero")
+
+# %% Leemos la tabla de información
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('ATP.csv', encoding = 'latin-1')
+print(df.head())
 
