6a57433f6ac55693653a4b3d18bfb151fd142709
ReinerCruz <a20196816@pucp.edu.pe>
2025-04-29 00:03:23 -0500

    Realizar actualización ejer1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,11 @@
-print("Hola mundo")
-print("Hola, còmo estas")
+import pandas as pd
+import numpy as np
+ 
+datos = {'Nombre': ['Leonardo', 'Reiner', 'Alex'],
+         'Materias': ['Lenguaje', 'Historia', 'Matemática'],
+         'Calificaciones': ['12', '20', '17'],
+         'Deportes': ['Natación', 'Tenis', 'Voley']
+         }
+
+df = pd.DataFrame(datos)
+print(df)
