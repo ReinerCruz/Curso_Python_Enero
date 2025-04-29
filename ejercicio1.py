ecd39c0b646c4f2eff62b2016b9a166705aed042
ReinerCruz <a20196816@pucp.edu.pe>
2025-04-29 00:11:50 -0500

    Realizar actualización 2

--- ejercicio1.py
+++ ejercicio1.py
@@ -9,3 +9,19 @@ datos = {'Nombre': ['Leonardo', 'Reiner', 'Alex'],
 
 df = pd.DataFrame(datos)
 print(df)
+print('\n'*4)
+
+# Datos no válidos
+
+datos2 = {'Nombre': ['Leonardo', 'Reiner', 'Alex'],
+         'Materias': ['Lenguaje', 'Historia', 'Matemática'],
+         'Calificaciones': ['12', np.nan, '17'],
+         'Deportes': ['Natación', 'N/A', 'Voley']
+         }
+
+
+df2 = pd.DataFrame(datos2)
+print(df2)
+print('\n'*1)
+print(df2.info())
+
