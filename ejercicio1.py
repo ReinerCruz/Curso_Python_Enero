a277054d59dd81b8d3e908c172169d08a6ab8ac0
ReinerCruz <a20196816@pucp.edu.pe>
2025-04-29 12:11:52 -0500

    Ignorar lectura CSV

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
  
+# %%
 datos = {'Nombre': ['Leonardo', 'Reiner', 'Alex'],
          'Materias': ['Lenguaje', 'Historia', 'Matemática'],
          'Calificaciones': ['12', '20', '17'],
@@ -11,9 +12,10 @@ df = pd.DataFrame(datos)
 print(df)
 print('\n'*4)
 
+# %%
 # Datos no válidos
 
-datos2 = {'Nombre': ['Leonardo', 'Reiner', 'Alex'],
+datos2 = {'Nombre': ['Leonardo', 'Reiner', 'N/A'],
          'Materias': ['Lenguaje', 'Historia', 'Matemática'],
          'Calificaciones': ['12', np.nan, '17'],
          'Deportes': ['Natación', 'N/A', 'Voley']
@@ -25,3 +27,28 @@ print(df2)
 print('\n'*1)
 print(df2.info())
 
+# %%
+# Estadísticas básicas
+
+df2.describe()
+nuevo = pd.DataFrame(datos2)
+nuevo = nuevo.replace(np.nan, 'O')
+print(nuevo)
+
+# %% Eliminar registros por columnas
+
+columna = df2[df2['Nombre'] != 'N/A']
+print(columna)
+print('\n'*1)
+
+columna2 = df2[~df2['Nombre'].isin(['Reiner', 'Leonardo'])] #Eliminar registros de varias filas de una vez
+print(columna2 )
+
+# %% Convertir los números a enteros. Recuerda que Calificaciones está como string
+
+df['Calificaciones'] = df.Calificaciones.astype(int)
+print(df.describe())
+
+# %% Estadísticas individuales
+
+print('El promedio de las calificaciones es', df['Calificaciones'].mean())
---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 29 11:52:58 2025
+
+@author: Mi Equipo
+"""
+
